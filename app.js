a85b8d5721a716e3d56188f39d5321a6a3d0bb3c
heeehennn <hendraaai0299@gmail.com>
2025-02-06 15:40:53 +0700

    update

--- app.js
+++ app.js
@@ -38,4 +38,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Set port
+const port = process.env.APP_PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 module.exports = app;
